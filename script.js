ee21051ccf8dadb211d992079556e0bec7f00b91
Apollo <166862370+z1Apollo@users.noreply.github.com>
2024-12-14 16:03:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const arroz = document.getElementById("arroz")
+const pate = document.getElementById("pate")
+const chester = document.getElementById("chester")
+const farofa = document.getElementById("farofa")
+
+pate.addEventListener("click", () => {
+    
+})
